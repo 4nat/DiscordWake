2e5fde960308b09101565c33e5270344a767bd8e
Harun MISTIK <har.mstk2015@gmail.com>
2020-07-20 13:13:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
+#THIS PROJECT MADE BY 4NAT
+#Discord : sehza#0001
+#ANY QUESTION > instagram/@ichbinharun  
 import requests
 import os 
 import time
-
-a=requests.get('https://peakhackersnet.glitch.me/')
-b=requests.get('https://peakhackersspot.glitch.me/')
-os.system('clear')
-if a.status_code == 200:
-    print('Netflix Request Success.')
-else:print('Spotify Request Fail!')
-if b.status_code == 200:
-    print('Spotify Request Success.')
-else:print('Spotify Request Fail!')
-
-time.sleep(180)
+count=0
+countt=0
+URL= input('Enter Your Glitch URL > ')
+sleep=int(input('Enter Time (Seconds) Default 180=3min > '))
+try:
+  while True:
+    os.system('clear')
+    count += 1
+    r=requests.get(URL)
+    if r.status_code == 200:
+        print(URL,'Request Success. = ',count,'Times')
+    else:countt += 1;print(URL,'Request Fail! = ',countt,'Times')
+    print('\nMADE BY 4NaT\n')
+    print('TOTAL SUCCESS = ',count)
+    print('TOTAL FAIL    = ',countt ) 
+    time.sleep(sleep)
+except:
+  print('Error :/')
+#</>#</>#</>#</>#</>#</>#</>#</>#</>#</>
